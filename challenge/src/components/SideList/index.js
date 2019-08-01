114e6ef71c2f228d4063afb8109e4dd9e1db4a11
Anna Leticia <anna.leticia@semprocesso.com.br>
2019-08-01 18:26:31 -0300

    AddAnimePage done and positionRelatives taken out
    Minor fixes to be done on AddAnimePage (see monday)

--- challenge/src/components/SideList/index.js
+++ challenge/src/components/SideList/index.js
@@ -21,26 +21,26 @@ class SideList extends React.Component {
             <div className = "sideListContainer" >
                 <div style={{display: (this.props.currentPage === "logIn") ? 'none' : '', width: '100%', height: '100%'}} >
                     <div className = "searchContainer">
-                        <div className = "searchByNameButton" onClick = { () => this.setState({searchBy : 'name'})} style={{backgroundColor:(this.state.searchBy === 'name') ? '#F0EBFF' : ''}} >
+                        <div className = "searchByButton" onClick = { () => this.setState({searchBy : 'name'})} style={{backgroundColor:(this.state.searchBy === 'name') ? '#F0EBFF' : ''}} >
                             <img src = {search} alt = "search"/>
                         </div>
-                        <div className = "searchByStarButton" onClick = { () => this.setState({searchBy : 'star'})} style={{backgroundColor:(this.state.searchBy === 'star') ? '#F0EBFF' : ''}} >
+                        <div className = "searchByButton" onClick = { () => this.setState({searchBy : 'star'})} style={{backgroundColor:(this.state.searchBy === 'star') ? '#F0EBFF' : ''}} >
                             <img src = {star} alt = "star"/>
                         </div>
                         <SearchBar onFormSubmit = {this.onSearchBarSubmit} searchByStar = {(this.state.searchBy === 'star')}/>
                     </div>
                     
                     <div className = "sortByContainer">
-                        <div className = "sortByNameButton" onClick = { () => this.setState({sortBy : 'name'})} style={{backgroundColor:(this.state.sortBy === 'name') ? '#F0EBFF' : ''}} >
+                        <div className = "sortByButton" onClick = { () => this.setState({sortBy : 'name'})} style={{backgroundColor:(this.state.sortBy === 'name') ? '#F0EBFF' : ''}} >
                             Name
                         </div>
-                        <div className = "sortByCategoryButton" onClick = { () => this.setState({sortBy : 'category'})} style={{backgroundColor:(this.state.sortBy === 'category') ? '#F0EBFF' : ''}} >
+                        <div className = "sortByButton" onClick = { () => this.setState({sortBy : 'category'})} style={{backgroundColor:(this.state.sortBy === 'category') ? '#F0EBFF' : ''}} >
                             Category
                         </div>
-                        <div className = "sortByDateButton" onClick = { () => this.setState({sortBy : 'date'})} style={{backgroundColor:(this.state.sortBy === 'date') ? '#F0EBFF' : ''}} >
+                        <div className = "sortByButton" onClick = { () => this.setState({sortBy : 'date'})} style={{backgroundColor:(this.state.sortBy === 'date') ? '#F0EBFF' : ''}} >
                             Date
                         </div>
-                        <div className = "sortByStarButton" onClick = { () => this.setState({sortBy : 'star'})} style={{backgroundColor:(this.state.sortBy === 'star') ? '#F0EBFF' : ''}} >
+                        <div className = "sortByButton" onClick = { () => this.setState({sortBy : 'star'})} style={{backgroundColor:(this.state.sortBy === 'star') ? '#F0EBFF' : ''}} >
                             Star
                         </div>
                     </div>
--- challenge/src/insideComponents/AddAnimePage/index.js
+++ challenge/src/insideComponents/AddAnimePage/index.js
@@ -1,12 +1,121 @@
 import React from 'react';
+import Dropdown from 'react-dropdown'
 
+import 'react-dropdown/style.css'
 import './index.less';
 
+import Stars from '../Stars/index';
+
 class AddAnimePage extends React.Component {
+    state = {name: '', category: '', nEpisodes: '', date: '', ranked: false, nStars: 0, image: '', synopsis: "", trailerUrl: ''};
+    categoryOptions = ['drama', 'isekai', 'shounen', 'shoujo'];
+
+    onFormSubmit = (event) => {
+        event.preventDefault(); //para impedir de atualizar a pagina depois de enivar o formulario
+    }
+
+    onSaveClick = () => {
+        //abrir popup
+        //mandar para o back os dados
+        this.setState({name: '', category: '', nEpisodes: '', date: '', ranked: false, nStars: 0, image: '', synopsis: "", trailerUrl: ''})
+    }
+
     render() {
         return (
             <div className="addAnimePageContainer">
-                AddAnimePage
+                <div className = "addInfoHeader">
+                    Add Anime Entry
+                </div>
+                <div className = "addInfoContainer">
+                    <div className = "fieldInfoContainer">
+                        <div className = "fieldNameContainer">
+                            Name:
+                        </div>
+                        <form className = "formContainer" onSubmit = {this.onFormSubmit}>
+                            <input 
+                                className = "addInfoInputContainer"
+                                type = "text" 
+                                value = {this.state.name} 
+                                onChange = {(event)=>{this.setState( { name : event.target.value } );}}
+                            />
+                        </form>
+                    </div>
+                    <div className = "fieldInfoContainer">
+                        <div className = "fieldNameContainer">
+                            Category:
+                        </div>
+                        <Dropdown 
+                            className = "categoryDropdown" 
+                            options={this.categoryOptions} 
+                            onChange={(event)=>{this.setState( { category : event.value } );}} 
+                            value={this.state.category} 
+                            placeholder="Select an option" 
+                        />
+                    </div>
+                    <div className = "fieldInfoContainer">
+                        <div className = "fieldNameContainer">
+                            Total episodes:
+                        </div>
+                        <form className = "formContainer" onSubmit = {this.onFormSubmit}>
+                            <input 
+                                className = "addInfoInputContainer"
+                                type = "text" 
+                                value = {this.state.nEpisodes} 
+                                onChange = {(event)=>{this.setState( { nEpisodes : event.target.value } );}}
+                            />
+                        </form>
+                    </div>
+                    <div className = "fieldInfoContainer">
+                        <div className = "fieldNameContainer">
+                            Synopsis:
+                        </div>
+                        <form className = "formContainer" onSubmit = {this.onFormSubmit}>
+                            <input 
+                                className = "addInfoInputContainer"
+                                type = "text" 
+                                value = {this.state.synopsis} 
+                                onChange = {(event)=>{this.setState( { synopsis : event.target.value } );}}
+                            />
+                        </form>
+                    </div>
+                    <div className = "fieldInfoContainer" >
+                        <div className = "fieldNameContainer">
+                            Rank:
+                        </div>
+                        <div className = "addStarContainer">
+                            <Stars nStars = {0} ranked = {true} />
+                        </div>
+                    </div>
+                    <div className = "fieldInfoContainer">
+                        <div className = "fieldNameContainer">
+                            Image Link:
+                        </div>
+                        <form className = "formContainer" onSubmit = {this.onFormSubmit}>
+                            <input 
+                                className = "addInfoInputContainer"
+                                type = "text" 
+                                value = {this.state.image} 
+                                onChange = {(event)=>{this.setState( { image : event.target.value } );}}
+                            />
+                        </form>
+                    </div>
+                    <div className = "fieldInfoContainer">
+                        <div className = "fieldNameContainer">
+                            Trailer youtube URL:
+                        </div> 
+                        <form className = "formContainer" onSubmit = {this.onFormSubmit}>
+                            <input 
+                                className = "addInfoInputContainer"
+                                type = "text" 
+                                value = {this.state.trailerUrl} 
+                                onChange = {(event)=>{this.setState( { trailerUrl : event.target.value } );}}
+                            />
+                        </form>
+                    </div>
+                    <div className = "saveButton" onClick = {this.onSaveClick}>
+                        Save
+                    </div>
+                </div>
             </div>
         );
     }
