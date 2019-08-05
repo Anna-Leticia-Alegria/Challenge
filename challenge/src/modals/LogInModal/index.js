c2231d646ad1e316b9df784a1ac371e62eef157a
Anna Leticia <anna.leticia@semprocesso.com.br>
2019-08-05 17:11:24 -0300

    Adding log in modal

--- challenge/src/modals/LogInModal/index.js
+++ challenge/src/modals/LogInModal/index.js
@@ -3,11 +3,95 @@ import React from 'react'
 import './index.less'
 
 class LogInModal extends React.Component {
+    state = {userName : '', lastName : '', birthday : '', email : '', password : '', confirmPassword : ''}
+
+    onFormSubmit = (event) => {
+        event.preventDefault()
+    }
+
+    onSaveClick = () => {
+        const {password, confirmPassword} = this.state
+        if(password === confirmPassword && password !== '') {
+            //enviar dados ao banco
+            this.props.closeModal()
+        }
+    }
+
     render () {
+        const {userName, lastName, birthday, email, password, confirmPassword} = this.state
         return (
-            <div onClick = {() => {this.props.closeModal()}}>
-                <div>oi</div>
-            </div>)
+            <div className = "newUserContainer">
+                <div className = "infoNewUserContainer">
+                    <label>Name: </label>
+                     <form className = "formNewUserContainer" onSubmit = {this.onFormSubmit}>
+                        <input 
+                            className = "newUserInputContainer"
+                            type = "text"
+                            value = {userName}
+                            onChange = {(event)=>{this.setState({userName : event.target.value})}}
+                        />
+                    </form>
+                </div>
+                <div className = "infoNewUserContainer">
+                    <label>Last Name: </label>
+                     <form className = "formNewUserContainer" onSubmit = {this.onFormSubmit}>
+                        <input 
+                            className = "newUserInputContainer"
+                            type = "text"
+                            value = {lastName} 
+                            onChange = {(event)=>{this.setState({lastName : event.target.value})}}
+                        />
+                    </form>
+                </div>
+                <div className = "infoNewUserContainer">
+                    <label>Birthday: </label>
+                     <form className = "formNewUserContainer" onSubmit = {this.onFormSubmit}>
+                        <input 
+                            className = "newUserInputContainer"
+                            type = "text" 
+                            value = {birthday}
+                            onChange = {(event)=>{this.setState({birthday : event.target.value})}}
+                        />
+                    </form>
+                </div>
+                <div className = "infoNewUserContainer">
+                    <label>Email: </label>
+                     <form className = "formNewUserContainer" onSubmit = {this.onFormSubmit}>
+                        <input 
+                            className = "newUserInputContainer"
+                            type = "text" 
+                            value = {email}
+                            onChange = {(event)=>{this.setState({email : event.target.value})}}
+                        />
+                    </form>
+                </div>
+                <div className = "infoNewUserContainer">
+                    <label>Password: </label>
+                     <form className = "formNewUserContainer" onSubmit = {this.onFormSubmit}>
+                        <input 
+                            className = "newUserInputContainer"
+                            type = "password" 
+                            value = {password}
+                            onChange = {(event)=>{this.setState({password : event.target.value})}}
+                        />
+                    </form>
+                </div>
+                <div className = "infoNewUserContainer">
+                    <label>Confirm Password: </label>
+                     <form className = "formNewUserContainer" onSubmit = {this.onFormSubmit}>
+                        <input 
+                            className = "newUserInputContainer"
+                            type = "password" 
+                            value = {confirmPassword}
+                            onChange = {(event)=>{this.setState({confirmPassword : event.target.value})}}
+                        />
+                    </form>
+                </div>
+                <div className = "newUserSaveButton" onClick = {this.onSaveClick} >
+                    save
+                </div>
+            </div>
+        )
     }
 }
 
